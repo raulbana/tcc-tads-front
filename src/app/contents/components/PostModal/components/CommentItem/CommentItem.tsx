fa56b395483fa1ae25bc3243f3d962ae38b3ec6d
AlissonGSantos <alisson.gab.santos@gmail.com>
2025-09-15 02:22:04 -0300

    feat: add separator between author name and date in CommentItem component

--- src/app/contents/components/PostModal/components/CommentItem/CommentItem.tsx
+++ src/app/contents/components/PostModal/components/CommentItem/CommentItem.tsx
@@ -55,6 +55,9 @@ const CommentItem: React.FC<CommentItemProps> = (props) => {
               <span className="font-medium text-gray-08 text-sm">
                 {comment.authorName}
               </span>
+
+              ·
+
               <span className="text-xs text-gray-07">
                 {formattedDate}
               </span>
